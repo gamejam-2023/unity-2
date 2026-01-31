3d36f92fae8633446cada812295cc9fe1ad259e2
Aron Ingi Lenvig <aronlenvigg@gmail.com>
2026-01-31 19:23:01 +0100

    fix gameStates

--- Assets/Scripts/GameStates.cs
+++ Assets/Scripts/GameStates.cs
@@ -2,31 +2,37 @@ using UnityEngine;
 
 public class GameStates : MonoBehaviour
 {
-
     public int score = 0;
     public float gameTime = 0f;
-    private float oldGameTime = 0f;
-    private int oldScoreCounter = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private int lastSecond = 0;
+    private int lastTenSecondMilestone = 0;
+
     void Start()
     {
         score = 0;
         gameTime = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         gameTime += Time.deltaTime;
-        // for every second passed, increase score by 1
-        if ((int)gameTime > (int)oldGameTime)
+
+        int currentSecond = Mathf.FloorToInt(gameTime);
+
+        // +1 score every second
+        if (currentSecond > lastSecond)
         {
-            // for every 10 seconds passed, increase score by an additional 1
-            oldScoreCounter += 1;
-            int extraScoreFromTime = ((int)gameTime / 10) - oldScoreCounter;
-            score += 1 + extraScoreFromTime;
-            oldGameTime = gameTime;
+            score += 1;
+            lastSecond = currentSecond;
         }
 
+        // +1 extra score every 10 seconds
+        int tenSecondMilestone = currentSecond / 10;
+        if (tenSecondMilestone > lastTenSecondMilestone)
+        {
+            score += 1;
+            lastTenSecondMilestone = tenSecondMilestone;
+        }
     }
 }
